ed0645977d3a3ece51be457c46f87282ea88bf11
Jared Harbison <jared.harbison@gmail.com>
2019-02-25 20:53:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,14 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     t = Time.new
     resp.write "#{t}\n"
-#    if 
+#    if
 #    else
 #    end
- 
+
     resp.finish
 #  end
- 
+
 end
