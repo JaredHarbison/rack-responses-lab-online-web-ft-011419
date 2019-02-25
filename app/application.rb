89569cab4d248c9917c2ac768d4b1531ff0faa23
Jared Harbison <jared.harbison@gmail.com>
2019-02-25 21:07:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    Time.now 
+    Time.now
     resp.write "#{Time.now.hour < 12 ? 'morning' : 'afternoon'}"
     resp.finish
   end
